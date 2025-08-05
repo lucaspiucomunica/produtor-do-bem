730fc363555d587d24d8b6a768d65340cbfa76b7
Lucas Sampaio <lucaspiucomunica@gmail.com>
2025-08-05 16:36:57 -0300

    feat(404): adiciona página de erro personalizada

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Página 404 - Página não encontrada
+ * 
+ * @package Produtor_do_Bem
+ */
+
+get_header();
+?>
+
+<main class="site-main">
+    <h1>404</h1>
+</main>
+
+<?php
+get_footer();
+?>
