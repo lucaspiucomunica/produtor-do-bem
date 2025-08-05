c1937858ead2494273431a9280bea25baea537e1
Lucas Sampaio <lucaspiucomunica@gmail.com>
2025-08-05 16:38:23 -0300

    style: reorganiza CSS e atualiza configuração Tailwind

--- src/libs/tailwind.config.js
+++ src/libs/tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Arquivos
   content: [
     '../../*',
     '../../src/js/*',
     '../../inc/**/*.php',
   ],
+
+  // Estilos
   theme: {
     extend: {
+      // Cores
       colors: {
-        primary: {
+        primario: {
           'principal': '#13863D',
           'variacao-1': '#117937',
           'variacao-2': '#0F6B31',
@@ -16,9 +20,44 @@ module.exports = {
           'variacao-4': '#0B5025',
           'variacao-5': '#0A431F',
         },
+        secundario: {
+          'principal': '#78BE21',
+          'variacao-1': '#6AA91F',
+          'variacao-2': '#5E961B',
+          'variacao-3': '#117937',
+          'variacao-4': '#538418',
+          'variacao-5': '#3B5E11',
+        },
+        neutro: {
+          'principal': '#F2EBE7',
+          'variacao-1': '#DAD4D0',
+          'variacao-2': '#C2BCB9',
+          'variacao-3': '#A9A5A2',
+          'variacao-4': '#918D8B',
+          'variacao-5': '#797674',
+          'variacao-6': '#29292B',
+          'branco': '#ffffff',
+          'preto': '#202022',
+        },
+        limao: {
+          'principal': '#E6F200',
+          'variacao-1': '#AEF50B',
+          'variacao-2': '#97DD05',
+        },
+      },
+
+      // Fontes
+      fontFamily: {
+        'body': ['Inter', 'sans-serif'],
+        'display': ['Sora', 'sans-serif'],
+      },
+
+      fontWeight: {
+        'regular': 400,
+        'semibold': 600,
+        'bold': 700,
       },
     },
   },
   plugins: []
 }
-
